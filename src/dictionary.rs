80786b1c87599be99c61002008d553a65f987de3
BittersweetPuff <biszkoptex@gmail.com>
2020-05-03 20:27:49 +0200

    Building lexicon form .wordlist files added

--- src/dictionary.rs
+++ src/dictionary.rs
@@ -2,24 +2,38 @@ use std::collections::HashMap;
 use std::io::BufReader;
 use std::io::BufRead;
 use std::fs::File;
+use std::path::Path;
 
 
-fn read_file_into_vector(filename: String) -> Vec<String> {
-    let f = File::open(filename).unwrap();
+fn read_file_into_vector(file: &Path) -> Vec<String> {
+    let f = File::open(file).unwrap();
     let file = BufReader::new(&f);
     let mut vector: Vec<String> = Vec::new();
 
     for line in file.lines() {
         let l = line.unwrap();
-        let word: String = l.trim().parse().expect("Błędna wartość");
+        let word: String = l.trim().parse().expect("Wrong value");
         vector.push(word);
     }
     vector
 }
 
+#[derive(Debug, PartialEq, Eq, Hash)]
+enum WordType {
+    PosNouns,
+    NegNouns,
+    PosAdj,
+    NegAdj,
+    PosComp,
+    NegComp,
+    ValidNames,
+    ZeroNouns,
+
+}
+
 #[derive(Debug)]
 pub struct Dictionary {
-    lexicon: HashMap<String, Vec<String>>,   
+    lexicon: HashMap<WordType, Vec<String>>,   
 }
 
 impl Dictionary {
@@ -29,7 +43,16 @@ impl Dictionary {
         lexicon: HashMap::new(),
         };
 
-        dict.lexicon.insert("Characters".to_string(), read_file_into_vector("language/test.wordlist".to_string()));
+        let path = Path::new("./lang/");
+
+        dict.lexicon.insert(WordType::PosNouns, read_file_into_vector(path.join("positive_noun.wordlist").as_path()));
+        dict.lexicon.insert(WordType::NegNouns, read_file_into_vector(path.join("negative_noun.wordlist").as_path()));
+        dict.lexicon.insert(WordType::PosAdj, read_file_into_vector(path.join("positive_adjective.wordlist").as_path()));
+        dict.lexicon.insert(WordType::NegAdj, read_file_into_vector(path.join("negative_adjective.wordlist").as_path()));
+        dict.lexicon.insert(WordType::PosComp, read_file_into_vector(path.join("positive_comparative.wordlist").as_path()));
+        dict.lexicon.insert(WordType::NegComp, read_file_into_vector(path.join("negative_comparative.wordlist").as_path()));
+        dict.lexicon.insert(WordType::ValidNames, read_file_into_vector(path.join("characters.wordlist").as_path()));
+        dict.lexicon.insert(WordType::ZeroNouns, read_file_into_vector(path.join("zero_nouns.wordlist").as_path()));
 
         dict
         
@@ -37,5 +60,9 @@ impl Dictionary {
 
     pub fn print(&self) {
         println!("{:#?}", self.lexicon)
+    }
+
+    pub fn print_keys(&self) {
+        println!("{:#?}", self.lexicon.keys())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 mod dictionary;
 pub use dictionary::*;
 
-
-
-
 fn main() {
 
     let dec = Dictionary::build();
     dec.print();
+    dec.print_keys();
 
 }
