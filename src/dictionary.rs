1e1c0f6e4d0e73a110b756d145528be614e6e524
BittersweetPuff <biszkoptex@gmail.com>
2020-05-08 00:27:14 +0200

    Added word checks

--- src/dictionary.rs
+++ src/dictionary.rs
@@ -1,10 +1,10 @@
 use std::collections::HashMap;
-use std::io::BufReader;
-use std::io::BufRead;
 use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
 use std::path::Path;
 
-
+///Loads values form text file to vector line by line (wordlist are suposed to be one word (or character name) per line).
 fn read_file_into_vector(file: &Path) -> Vec<String> {
     let f = File::open(file).unwrap();
     let file = BufReader::new(&f);
@@ -28,34 +28,55 @@ enum WordType {
     NegComp,
     ValidNames,
     ZeroNouns,
-
 }
 
 #[derive(Debug)]
 pub struct Dictionary {
-    lexicon: HashMap<WordType, Vec<String>>,   
+    lexicon: HashMap<WordType, Vec<String>>,
 }
 
 impl Dictionary {
     pub fn build() -> Dictionary {
-
         let mut dict = Dictionary {
-        lexicon: HashMap::new(),
+            lexicon: HashMap::new(),
         };
 
         let path = Path::new("./lang/");
 
-        dict.lexicon.insert(WordType::PosNouns, read_file_into_vector(path.join("positive_noun.wordlist").as_path()));
-        dict.lexicon.insert(WordType::NegNouns, read_file_into_vector(path.join("negative_noun.wordlist").as_path()));
-        dict.lexicon.insert(WordType::PosAdj, read_file_into_vector(path.join("positive_adjective.wordlist").as_path()));
-        dict.lexicon.insert(WordType::NegAdj, read_file_into_vector(path.join("negative_adjective.wordlist").as_path()));
-        dict.lexicon.insert(WordType::PosComp, read_file_into_vector(path.join("positive_comparative.wordlist").as_path()));
-        dict.lexicon.insert(WordType::NegComp, read_file_into_vector(path.join("negative_comparative.wordlist").as_path()));
-        dict.lexicon.insert(WordType::ValidNames, read_file_into_vector(path.join("characters.wordlist").as_path()));
-        dict.lexicon.insert(WordType::ZeroNouns, read_file_into_vector(path.join("zero_nouns.wordlist").as_path()));
+        dict.lexicon.insert(
+            WordType::PosNouns,
+            read_file_into_vector(path.join("positive_noun.wordlist").as_path()),
+        );
+        dict.lexicon.insert(
+            WordType::NegNouns,
+            read_file_into_vector(path.join("negative_noun.wordlist").as_path()),
+        );
+        dict.lexicon.insert(
+            WordType::PosAdj,
+            read_file_into_vector(path.join("positive_adjective.wordlist").as_path()),
+        );
+        dict.lexicon.insert(
+            WordType::NegAdj,
+            read_file_into_vector(path.join("negative_adjective.wordlist").as_path()),
+        );
+        dict.lexicon.insert(
+            WordType::PosComp,
+            read_file_into_vector(path.join("positive_comparative.wordlist").as_path()),
+        );
+        dict.lexicon.insert(
+            WordType::NegComp,
+            read_file_into_vector(path.join("negative_comparative.wordlist").as_path()),
+        );
+        dict.lexicon.insert(
+            WordType::ValidNames,
+            read_file_into_vector(path.join("characters.wordlist").as_path()),
+        );
+        dict.lexicon.insert(
+            WordType::ZeroNouns,
+            read_file_into_vector(path.join("zero_nouns.wordlist").as_path()),
+        );
 
         dict
-        
     }
 
     pub fn print(&self) {
@@ -64,5 +85,81 @@ impl Dictionary {
 
     pub fn print_keys(&self) {
         println!("{:#?}", self.lexicon.keys())
+    }
+
+    ///Check if word is a noun.
+    pub fn is_noun(&self, word: String) -> bool {
+        self.lexicon
+            .get(&WordType::PosNouns)
+            .unwrap()
+            .to_vec()
+            .iter()
+            .any(|v| v == &word)
+            || self
+                .lexicon
+                .get(&WordType::NegNouns)
+                .unwrap()
+                .to_vec()
+                .iter()
+                .any(|v| v == &word)
+    }
+
+    ///Check if word is an adjective.
+    pub fn is_adjective(&self, word: String) -> bool {
+        self.lexicon
+            .get(&WordType::PosAdj)
+            .unwrap()
+            .to_vec()
+            .iter()
+            .any(|v| v == &word)
+            || self
+                .lexicon
+                .get(&WordType::NegAdj)
+                .unwrap()
+                .to_vec()
+                .iter()
+                .any(|v| v == &word)
+    }
+
+    ///Check if word is a comparative.
+    pub fn is_comparative(&self, word: String) -> bool {
+        self.lexicon
+            .get(&WordType::PosComp)
+            .unwrap()
+            .to_vec()
+            .iter()
+            .any(|v| v == &word)
+            || self
+                .lexicon
+                .get(&WordType::NegComp)
+                .unwrap()
+                .to_vec()
+                .iter()
+                .any(|v| v == &word)
+    }
+
+    ///Returns a value of a noun form a range (-1, 1).
+    pub fn noun_value(&self, noun: String) -> i8 {
+        if self
+            .lexicon
+            .get(&WordType::PosNouns)
+            .unwrap()
+            .to_vec()
+            .iter()
+            .any(|v| v == &noun)
+        {
+            1
+        } else if self
+            .lexicon
+            .get(&WordType::NegNouns)
+            .unwrap()
+            .to_vec()
+            .iter()
+            .any(|v| v == &noun)
+        {
+            -1
+        } else {
+            0
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,21 @@ mod dictionary;
 pub use dictionary::*;
 
 fn main() {
-
     let dec = Dictionary::build();
-    dec.print();
-    dec.print_keys();
 
+    println!("{}", dec.is_noun("belh".to_string()));
+    println!("{}", dec.is_noun("beggar".to_string()));
+    println!("{}", dec.is_noun("angel".to_string()));
+
+    println!("{}", dec.is_adjective("belh".to_string()));
+    println!("{}", dec.is_adjective("cursed".to_string()));
+    println!("{}", dec.is_adjective("golden".to_string()));
+
+    println!("{}", dec.is_comparative("belh".to_string()));
+    println!("{}", dec.is_comparative("punier".to_string()));
+    println!("{}", dec.is_comparative("fresher".to_string()));
+
+    println!("{}", dec.noun_value("beggar".to_string()));
+    println!("{}", dec.noun_value("angel".to_string()));
+    println!("{}", dec.noun_value("belh".to_string()));
 }
